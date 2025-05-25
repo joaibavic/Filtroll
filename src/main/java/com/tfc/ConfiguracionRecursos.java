76156717be1af5756d3cf39139f5ad0a6a6f158b
Joaquín Ibáñez Vicente <jivpc185@gmail.com>
2025-05-25 12:19:22 +0200

    Añadido handler para imágenes predefinidas en ConfiguracionRecursos

--- src/main/java/com/tfc/ConfiguracionRecursos.java
+++ src/main/java/com/tfc/ConfiguracionRecursos.java
@@ -6,11 +6,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ConfiguracionRecursos implements WebMvcConfigurer {
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Recursos generados dinámicamente desde el sistema de archivos
         registry.addResourceHandler("/imagenes/resultados/**")
                 .addResourceLocations("file:imagenes/resultados/")
                 .setCachePeriod(0);
+
+        // Recursos predefinidos incluidos en el JAR
+        registry.addResourceHandler("/imagenes/predefinidas/**")
+                .addResourceLocations("classpath:/static/imagenes/predefinidas/")
+                .setCachePeriod(0);
+
         System.out.println("🛠 ConfiguracionRecursos ACTIVADA");
     }
 }
