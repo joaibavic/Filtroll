934d7db4dc39b2098ee7af1ed2249e075e754189
Joaquín Ibáñez Vicente <jivpc185@gmail.com>
2025-05-25 10:23:21 +0200

    Confirmado: Dockerfile crea carpeta imagenes/resultados y cambio de margo-general.html / ConfiguracionRecursos

--- src/main/java/com/tfc/configuracion/ConfiguracionRecursos.java
+++ src/main/java/com/tfc/configuracion/ConfiguracionRecursos.java
@@ -1,4 +1,4 @@
-package com.tfc;
+package com.tfc.configuracion;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -6,12 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ConfiguracionRecursos implements WebMvcConfigurer {
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Sirve las imágenes generadas en tiempo de ejecución desde la carpeta externa
         registry.addResourceHandler("/imagenes/resultados/**")
                 .addResourceLocations("file:imagenes/resultados/")
-                .setCachePeriod(0); // Cache desactivada para mostrar la última versión
+                .setCachePeriod(0);
+        System.out.println("🛠 ConfiguracionRecursos ACTIVADA");
     }
 }
